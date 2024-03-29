5ad6944b590c4f692b13eda115f9a4437ea54efe
Amit Gupta <amitg@VF096.local>
2024-03-29 21:14:19 +0530

    changes added for nestjs ecommerce api design

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'config';
+
+
+@Module({
+  imports: [MongooseModule.forRoot(config.get('mongodbUrl'))],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
